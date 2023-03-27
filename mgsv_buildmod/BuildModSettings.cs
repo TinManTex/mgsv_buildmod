aaa7fcfc050fc565152b3823990ea6aab484d3b8
TinManTex <tinman.squad@gmail.com>
2023-03-28 09:48:28 +1300

    bring over comments from ih buildsettings

--- mgsv_buildmod/BuildModSettings.cs
+++ mgsv_buildmod/BuildModSettings.cs
@@ -5,30 +5,58 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace mgsv_buildmod {
+    //tex deserialized from json build mod settings file
+    //supplies the paths and drives most of the behavior of mgsv_buildmod
+    //defaults are just to give user some idea of values when WriteDefaultConfigJson serializes this.
+    //though it's probably better for user to look at Infinite Heavens buildSettings file on its repo.
+    //most comments below are from the commented ih buildsettings
+
+    // Paths can either be absolute or relative, to what depends on what the path parameter is,
+    // but the majority they are relative to modPath.
+    // If supplying relative path remember not to include any leading slashes
+    // Slash direction doesnt really matter, but remember if you have windows style backslashes \ you need to escape them ex c:\somepath\subfolder\ = "c:\\somepath\\subfolder\\"
+    // relative paths should not have leading slashes at the start
+    // folder paths should have trailing slashes at the end
+
     internal class BuildModSettings {
-        //tex smakebite metadata
+        // Snakebite metadata
         public string Version = "";
         public string Name = "";
         public string Author = "";
         public string Website = "";
+        //
 
-        public string modFileName = "Infinite Heaven";//tex .mgsv name
-        public string readMeFileName = "Readme.txt";//STRUCURE inside docs path
-        public string docsPath = @"C:\Projects\MGS\InfiniteHeaven\tpp\gamedir-ih\GameDir\mod\docs\Infinite Heaven";
-        public string metadataPath = @"C:\Projects\MGS\InfiniteHeaven\tpp";
+        // Root for relative paths within source mod layout, 
+        // if this parameter is not set then modPath will be set to the path of this buildSettings file.
+        public string modPath = null; //"C:/Projects/MGS/InfiniteHeaven/tpp";
 
-        public string modPath = null; //@"C:\Projects\MGS\InfiniteHeaven\tpp";//tex root for relative paths within source mod layout, if null or empty modPath will be set to the path of the given buildSettings file
+        // Folder all the following is copied to before being makebitten.
+        public string makebiteBuildPath = "C:/Projects/MGS/build/infiniteheaven/makebite";
+        // Folder the built .mgsv and docsPath files are copied to.
+        public string buildPath = "C:/Projects/MGS/build/infiniteheaven/build";
 
-        //tex folders have various tools run on them (see buildFox2s etc settings)
-        //then are copied outright to makebitepath
-        ///so need to be in makebiteable layout
-        //GOTCHA: don't fill this out with example because json entries are added rather than replace
+        // .mgsv name, copied to buildPath 
+        public string modFileName = "Infinite Heaven";
+        // Name of file inside docsPath, file is copied to makebiteBuildPath/readme.txt so makebite can pull it into metadata. 
+        public string readMeFileName = "Readme.txt";
+        // For copyDocsToBuild, copied to {buildPath}\docs
+        public string docsPath = "gamedir-ih/GameDir/mod/docs/Infinite Heaven";
+        // Path to metadata.xml for snakebite, copied to makebiteBuidlPath, edited with above values. 
+        // Default relative to modPath. 
+        // You can just start by copying any metadata.xml
+        // Doesn't matter what anything in the metadata.xml is set to except for MGSVersion. Everything else will get filled out by the above values.
+        public string metadataPath = "";
+
+        //GOTCHA: don't fill out the following structures with examples because json entries are added rather than replace
+
+        // copyModFolders: Folders entirely copied to makebiteBuildPath
         public List<string> modFolders = new List<string> {
             //REF
             //"fpk-mod/",
             //"fpk-mod-ih/",
         };
 
+        // copyModFiles: Specific files for folders copied to makebiteBuildPath
         public Dictionary<string, List<string>> modFiles = new Dictionary<string, List<string>>() {
             //REF
             //{"data1_dat-lua-ih/", new List<string>{
@@ -42,7 +70,7 @@ namespace mgsv_buildmod {
             // },
         };//modFiles
 
-        //copyModArchiveFiles
+        // copyModArchiveFiles: Lets you keep files in Assetspath structure, but then have them put in their archive folders in makebiteBuildPath
         public Dictionary<string,
             Dictionary<string, List<string>>> modArchiveFiles = new Dictionary<string,
                                                                     Dictionary<string, List<string>>>() {
@@ -52,19 +80,24 @@ namespace mgsv_buildmod {
                 //}, },
             };//modArchiveFiles
 
+        public string externalLuaPath = @"C:\Projects\MGS\InfiniteHeaven\tpp\gamedir-ih\GameDir\mod";//tex for copyExternalLuaToInternal
+ 
+        //CULL
         public string luaFpkdFilesPath = @"C:\Projects\MGS\InfiniteHeaven\tpp\fpkd-combined-lua";//tex for copyLuaFpkdFiles //CULL supersceded by copyModArchiveFiles
 
-        public string externalLuaPath = @"C:\Projects\MGS\InfiniteHeaven\tpp\gamedir-ih\GameDir\mod";//tex for copyExternalLuaToInternal
         public string modulesLuaPath = @"C:\Projects\MGS\InfiniteHeaven\tpp\gamedir-ih\GameDir\mod\modules";//tex for copyModulesToInternal
         public string modulesInternalPath = @"\Assets\tpp\script\ih";//tex for copyModulesToInternal
 
-        public string makebiteBuildPath = @"C:\Projects\MGS\build\infiniteheaven\makebite"; //tex: where the various files are actually pulled together before being makebitten
-        public string buildPath = @"C:\Projects\MGS\build\infiniteheaven\build";//tex where the built .mgsv and docs folder are placed ready for being zipped for release //TODO: zip it too
-
         public string gamePath = null;//@"C:\Games\Steam\SteamApps\common\MGS_TPP";
 
-        public bool copyDocsToBuild = true;//tex copies docsPath to build, so they can be included in release zip for user to check out without installing or unzipping .mgsv
+        // Settings 
 
+        // Copies docsPath to {buildPath}\docs, so they can be included in release zip for user to check out without installing or unzipping .mgsv
+        public bool copyDocsToBuild = true;
+
+        // If you dont have actual translations for lang codes this will copy the eng lng2s to the other lang code lng2s.
+        // Currently only does for lang_default_data_eng_fpk (folder) > lang_default_data_{langCode}_fpk 
+        // Should not be run if you are modding base game lngs.
         public bool copyEngLng2sToOtherLangCodes = false;//tex if you dont have actual translations for lang codes this will copy the eng lng2s to the other lang code lng2s
 
         public bool compileMakebiteBuildFiles = true; //tex overall switch of below
@@ -91,28 +124,29 @@ namespace mgsv_buildmod {
         };
 
         public bool copyLuaFpkdFiles = false;//CULL supersceded by copyModArchiveFiles //tex uses luaFpkdFilesPath, fpk internal pathed lua files, their DOBUILD comment headers are used to copy them to full fpk paths, 
-        
+
+        public bool copyModulesToInternal = false;//copies external lua modules to internal [WIP]
+
         public bool copyModFolders = true;//tex uses modFolders
 
         public bool copyModFiles = true;//for modFiles
 
         public bool copyModArchiveFiles = true;//for modArchiveFiles
 
-        //tex copies externalLuaPath to internal, intended for release. So you can develop using IHs external (gamedir\mod\<in-dat path>), and then copy them in to in-dat for release
-        //WARNING: ih will still try to load external by default, so do not include externalLuaPath files in gamedir-mod\release)
-        //uses externalLuaPath
+        // Copies files in externalLuaPath > "Assets", "Fox", "shaders", "Tpp" to the equivalent makebiteBuildPath
+        // ie I keep some internal files external in \mod\ for dev build, but are put back internal for release
+        // GOTCHA: ih will still try to load external by default, so do not include externalLuaPath files in release (see ih repo tpp vs tpp-release)
+        // The function will delete any of the above mentioned sub paths in makebiteBuild GameDir\mod\ to mitigate this.
         public bool copyExternalLuaToInternal = false;
-
-        public bool copyModulesToInternal = false;//copies external lua modules to internal [WIP]
-
-        public bool makeMod = true; //tex run makebite on built mod
-        //tex (if installmod), uninstall previous mod, false will just install over top,
-        //which may save some time, only issue may be if you removed some files in the new version
-        //GOTCHA: uninstall only uninstalls 1st entry of Name found (multiple mods of same name may be installed, see GOTCHA below)
+        // Run makebite on makebiteBuildPath, copy the .mgsv to buildPath.
+        public bool makeMod = true;
+        // Uninstalls first mod found that matches {Name}
+        // for development it's usually fine to leave this false providing you arent removing files between build versions
+        // it will just keep adding mod entries in snakebite though, so eventually you'll have to uninstall them manually.
         public bool uninstallExistingMod = true;
+        // Install .mgsv file that was built during makeMod
         public bool installMod = true;//tex install build mod
-        //GOTCHA: snakebite will actually add new mod entry for every install, installing with the same mod Name does not overwrite that entry (even though it overwrites files)
-        //TODO: snakebite: possibly an overwritemodofsamename option
-        public bool waitEnd = true;//tex leaves program window open when done, with press any key to exit
+        // Leave the program window at the end open, waiting for any keypress to close
+        public bool waitEnd = true;
     }//BuildModSettings
 }//mgsv_buildmod
